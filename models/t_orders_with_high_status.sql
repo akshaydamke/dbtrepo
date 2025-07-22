f154c2397fa61d7bc31498b10d20449c1cb9b2a3
akshaydamke <akshaytvmsts@gmail.com>
2025-07-22 11:29:00 +0000

    changes made to repository

---
+++ models/t_orders_with_high_status.sql
@@ -0,0 +1,5 @@
+select * from MARKETING_DB.MARKETING_SCHEMA.T_ORDERS
+where o_orderpriority='2-HIGH'
+and o_orderstatus='O'
+and date_part("MM",O_ORDERDATE)=1
+and date_part("YYYY",O_ORDERDATE)=1997
