c8e2a950a95c0c6d8ecc028f9b08c54bb1c38fdb
akshaydamke <akshaytvmsts@gmail.com>
2025-07-22 14:01:43 +0000

    created t_customers & t_orders

---
+++ models/t_customers.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+with customers as (
+select
+customer_id as customer_id,
+first_name,
+last_name
+from marketing_db.marketing_schema.customers
+)
+select * from customers
